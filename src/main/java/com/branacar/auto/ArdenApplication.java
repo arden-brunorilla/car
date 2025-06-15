4c85939f43da86ca720e136f60fa969561af6e51
brunorilla <brunorilla9@gmail.com>
2025-06-15 18:15:14 -0300

    fixing eureka sync errors

--- src/main/java/com/branacar/auto/ArdenApplication.java
+++ src/main/java/com/branacar/auto/ArdenApplication.java
@@ -18,17 +18,13 @@ public class ArdenApplication {
         SpringApplication.run(ArdenApplication.class, args);
     }
 
-    /**
-     * Inserta un Car de ejemplo con un VIN fijo para que puedas
-     * probar rápidamente el endpoint GET /cars/{id}.
-     */
     @Bean
     CommandLineRunner seedCar(CarRepository repo) {
         return args -> {
             repo.save(
                     Car.builder()
                             .carId(UUID.fromString("11111111-1111-1111-1111-111111111111"))
-                            .vin("VIN-TEST-001")                    // ← tu VIN de prueba
+                            .vin("VIN-TEST-001")
                             .status(CarStatus.IN_STOCK)
                             .listPrice(new BigDecimal("25000000"))
                             .build()
