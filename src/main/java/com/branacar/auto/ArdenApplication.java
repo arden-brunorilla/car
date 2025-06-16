a33e0e2364658df8f40f7c211b9c5ea87cc0f66d
brunorilla <brunorilla9@gmail.com>
2025-06-16 18:29:45 -0300

    added new seed + h2 ui support + tested catalog API - integration with stock-service remaining

--- src/main/java/com/branacar/auto/ArdenApplication.java
+++ src/main/java/com/branacar/auto/ArdenApplication.java
@@ -17,18 +17,4 @@ public class ArdenApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArdenApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner seedCar(CarRepository repo) {
-        return args -> {
-            repo.save(
-                    Car.builder()
-                            .carId(UUID.fromString("11111111-1111-1111-1111-111111111111"))
-                            .vin("VIN-TEST-001")
-                            .status(CarStatus.IN_STOCK)
-                            .listPrice(new BigDecimal("25000000"))
-                            .build()
-            );
-        };
-    }
 }
